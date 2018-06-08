7dcf093f984ad33e51485ab7cacdb7b47a8d6d1e
krmayank911 <krmayank911@gmail.com>
2018-06-08 23:42:47 +0530

    updated custom toolbar

--- app/src/main/java/com/buggyarts/customviews/customViews/SimpleToolBar.java
+++ app/src/main/java/com/buggyarts/customviews/customViews/SimpleToolBar.java
@@ -3,8 +3,11 @@ package com.buggyarts.customviews.customViews;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.PopupMenu;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -13,10 +16,11 @@ import android.widget.TextView;
 
 import com.buggyarts.customviews.R;
 
-public class SimpleToolBar extends FrameLayout implements View.OnClickListener{
+public class SimpleToolBar extends FrameLayout implements View.OnClickListener, PopupMenu.OnMenuItemClickListener{
 
     private TextView titleLabel;
     private ImageView backButton;
+    private ImageView overflowButton;
     private RelativeLayout backGroundView;
 
     private TopBarCallback topCallback;
@@ -38,12 +42,17 @@ public class SimpleToolBar extends FrameLayout implements View.OnClickListener{
 
         backButton = findViewById(R.id.backButton);
         backButton.setOnClickListener(this);
+
+        overflowButton = findViewById(R.id.overflowButton);
+        overflowButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         if(view.getId() == R.id.backButton){
             topCallback.backButtonCalled();
+        }else if(view.getId() == R.id.overflowButton){
+            showPopup(view);
         }
     }
 
@@ -62,4 +71,26 @@ public class SimpleToolBar extends FrameLayout implements View.OnClickListener{
     public TextView getTitleLabel() {
         return titleLabel;
     }
+
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(getContext(), v);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.menu, popup.getMenu());
+        popup.show();
+
+        popup.setOnMenuItemClickListener(this);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_to_favourites:
+                return true;
+            case R.id.save_to_device:
+                return true;
+            default:
+                return false;
+        }
+    }
+
 }
